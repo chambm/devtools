ece35a74491b8fcc2f215700afc408a1df6902fc
Matt Chambers <matt.chambers42@gmail.com>
2016-09-08 11:10:29 -0500

    Update easy_install.R

--- R/easy_install.R
+++ R/easy_install.R
@@ -1,8 +1,4 @@
-## Mirrors: uncomment the following and change to your favorite CRAN mirror
-## if you don't want to use the default (cran.rstudio.com).
-## options("repos" = c(CRAN="https://cran.rstudio.com"))
-
 local({
-  install.packages("devtools")
+  install.packages("devtools", repos=c(CRAN="https://cran.rstudio.com"))
   devtools::install_github("hadley/devtools")
 })
